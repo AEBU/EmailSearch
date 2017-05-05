b27bba4e7bc24a2285064b4086ebd2bc7cd0cd61
Alexis Bautista <rhack_100@outlook.com>
2017-05-05 10:59:00 -0500

    :init

---
+++ app/src/main/java/lex/uce/edu/emailsearch/EmailMainActivity.java
@@ -0,0 +1,13 @@
+package lex.uce.edu.emailsearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmailMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_main);
+    }
+}
